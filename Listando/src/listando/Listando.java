471a56a1a0098a5b51e238ec99727646c46fb913
Filipe Barros <barros7@icloud.com>
2017-03-22 16:14:54 -0300

    Listando

---
+++ Listando/src/listando/Listando.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listando;
+
+/**
+ *
+ * @author FilipeBarros
+ */
+public class Listando {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i;
+        
+        
+        for(i=0;i<21;i++) {
+            System.out.println(+i);
+        }
+        
+        
+        
+    }
+    
+}
