62b2664c94c84e5bb3cd12233f0ef523f98a6a6b
Filipe Barros <barros7@icloud.com>
2017-03-22 16:48:35 -0300

    i

---
+++ SomaImpares/src/somaimpares/SomaImpares.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package somaimpares;
+
+/**
+ *
+ * @author FilipeBarros
+ */
+public class SomaImpares {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Imprimindo dos os números ípares de 0 a 100");
+        int i,x,n1 = 0;
+    
+    for(i=0;i<100;i++) {
+        
+        if(((i%2) == 0 )) {
+          }
+        else {
+             n1=n1+i;
+            System.out.println("Numeros Ímpares:" +n1);
+               
+         }
+    }
+    
+}
+}
