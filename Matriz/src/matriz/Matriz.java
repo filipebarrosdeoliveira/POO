84ea3d60b124a220995a0141fdfd1a20d8277c6b
Filipe Barros <barros7@icloud.com>
2017-03-27 14:50:10 -0300

    Matriz

---
+++ Matriz/src/matriz/Matriz.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matriz;
+
+/**
+ *
+ * @author FilipeBarros
+ */
+public class Matriz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int coluna = 0;
+        int linha = 0;
+    
+        double [][] matriz = new double[linha][coluna];
+        
+        for (int linha = 0; linha &lt linhas; linha++) {
+          for (int coluna = 0; coluna &lt colunas; coluna++) {
+             matriz [linha] [coluna] = Math.random();
+    }
+}
+    
+    
+    }
+    
+}
