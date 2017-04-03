76ca2156333a55a71a34fa067fe122e3836e7fe8
Filipe Barros <barros7@icloud.com>
2017-04-03 18:03:52 -0300

    aluno-turma

---
+++ aluno-turma/src/aluno/turma/Disciplina.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aluno.turma;
+
+/**
+ *
+ * @author FilipeBarros
+ */
+
+public class Disciplina {
+
+    private String curso1,curso2;
+    
+    public void setCurso1 (String curso1) {
+        this.curso1 = curso1;
+    }
+    
+    public String getCurso1 () {
+        return curso1;
+    }
+    
+     public void setCurso2 (String curso2) {
+        this.curso2 = curso2;
+    }
+    
+    public String getCurso2 () {
+        return curso2;
+    }
+    
+}
---
+++ aluno-turma/src/aluno/turma/Turma.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aluno.turma;
+
+import java.util.*;
+
+public class Turma {
+  LinkedList alunos;
+  int max;
+
+  public Turma(int max) {
+     this.max = max;
+     alunos = new LinkedList();
+  }
+
+  public boolean adicionaAluno(Aluno aluno) {
+
+     boolean result;
+
+     if ( this.alunos.size() == this.max )
+        result = false;
+     else
+        result = alunos.add(aluno);
+     
+     return result;
+  }
+     
+  public boolean removeAluno(Aluno aluno) {
+     
+     return alunos.remove(aluno);  
+  
+  }
+
+  public Collection getAlunos() {
+     return this.alunos;
+  }
+
+  public double media() {
+
+     double soma = 0;
+    
+     Iterator i = this.alunos.iterator();
+     while ( i.hasNext() ) {
+        Aluno aluno = (Aluno) i.next();
+        soma += aluno.media();
+     }
+
+     return ( soma / alunos.size() );
+
+  }
+
+  public Aluno maiorMedia() {
+
+     Iterator i = alunos.iterator();
+
+     Aluno melhorAluno = (Aluno) i.next();
+     double maior = melhorAluno.media();
+
+     while ( i.hasNext() ) {
+        Aluno aluno = (Aluno) i.next();
+        if ( aluno.media() > maior ) {
+           melhorAluno = aluno;
+           maior = aluno.media();
+        }
+     }
+     
+     return melhorAluno;
+  }
+
+  public Aluno menorMedia() {
+ 
+     Iterator i = alunos.iterator();
+
+     Aluno piorAluno = (Aluno) i.next();
+     double menor = piorAluno.media();
+
+     while ( i.hasNext() ) {
+        Aluno aluno = (Aluno) i.next();
+        if ( aluno.media() > menor ) {
+           piorAluno = aluno;
+           menor = aluno.media();
+        }
+     }
+
+     return piorAluno;
+  }
+}
